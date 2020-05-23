516589dc41757cf74be8f4f7e1dd78de6ed815c5
Rich Posert <rich@posert.com>
2020-05-23 16:26:14 -0700

    More checks on dir

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
 
 def who_in_lab(workspace):
+    if not os.path.exists(workspace):
+        os.makedirs(workspace)
+        
     present_members = ', '.join([f'<@{x}>' for x in os.listdir(workspace)])
     if present_members == '':
         present_members = 'nobody'
@@ -36,29 +39,28 @@ def check_in(direction, user_name, user_id, workspace):
             return f'Can\'t check out if you never came in, {user_name}'
 
 def _event_handler(event_type, slack_event):
-    team_id = slack_event['team_id']
 
     if event_type == 'message':
         if 'bot_id' not in slack_event['event']:
+            team_id = slack_event['team_id']
             message_text = slack_event['event']['text'].lower()
             user_id = slack_event['event']['user']
             user_name = f'<@{user_id}>'
             channel = slack_event['event']['channel']
-            workspace = slack_event['team_id']
             if 'who' in message_text:
                 response = slack_web_client.chat_postMessage(
                     channel = channel,
-                    text = who_in_lab(workspace)
+                    text = who_in_lab(team_id)
                 )
             elif ' in' in message_text:
                 response = slack_web_client.chat_postMessage(
                     channel = channel,
-                    text = check_in('in', user_name, user_id, workspace)
+                    text = check_in('in', user_name, user_id, team_id)
                 )
             elif ' out' in message_text:
                 response = slack_web_client.chat_postMessage(
                     channel = channel,
-                    text = check_in('out', user_name, user_id, workspace)
+                    text = check_in('out', user_name, user_id, team_id)
                 )
 
         return make_response('Read a message', 200, )
