e80e11841486b2de6733c424da6f85368ace345e
Rich Posert <rich@posert.com>
2020-05-23 13:42:19 -0700

    Responds to mentions twice

--- app.py
+++ app.py
@@ -1,51 +1,44 @@
 import os
 import logging
-from flask import Flask
+from flask import Flask, request, make_response, render_template
 from slack import WebClient
+from slack.errors import SlackApiError
 from slackeventsapi import SlackEventAdapter
 from quarantine_tracker import GotItMessage
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
+SocketIO(app, cors_allowed_origins='*')
+slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
 
-SocketIO(app, cors_allowed_origins='*')
 
-# Initialize a Web API client
-slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+def _event_handler(event_type, slack_event):
+    print(slack_event)
+    team_id = slack_event['team_id']
 
-# App data
-members_in: []
-members_out: []
+    if event_type == 'app_mention':
+        ts = slack_event['event']['ts']
+
+        response = slack_web_client.chat_postMessage(
+            channel = 'C013ZC50SPQ',
+            text = f'You mentioned me directly at timestamp {ts}'
+        )
+        return make_response('Got the mention', 200,)
+
+@app.route('/', methods = ['GET', 'POST'])
+def process_request():
+    slack_event = request.get_json()
+
+    if 'challenge' in slack_event:
+        return make_response(slack_event['challenge'], 200, {'content_type': 'application/json'})
+
+    if 'event' in slack_event:
+        event_type = slack_event['event']['type']
+
+        return _event_handler(event_type, slack_event)
 
 
-@app.route('/')
-def hello():
-    return 'Hello world!'
-
-
-def got_it(user_id: str, channel: str):
-
-    got_it = GotItMessage(channel)
-
-    message = got_it.get_message_payload()
-
-    slack_web_client.chat_postMessage(**message)
-
-
-@slack_events_adapter.on("message")
-def message(payload):
-    """Display the onboarding welcome message after receiving a message
-    that contains "start".
-    """
-    event = payload.get("event", {})
-
-    channel_id = event.get("channel")
-    user_id = event.get("user")
-    text = event.get("text")
-    print('Got a message')
-
-    return got_it(user_id, channel_id)
 
 
 if __name__ == "__main__":
