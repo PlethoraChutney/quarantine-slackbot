f91d83a1ebf7d3d5d8a137a8f78fd9291682579c
Rich Posert <rich@posert.com>
2020-05-23 14:32:18 -0700

    Working on keeping track of lab members

--- app.py
+++ app.py
@@ -13,18 +13,58 @@ slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
 
 
+in_lab = []
+out_lab = []
+
+def who_in_lab():
+    if in_lab:
+        present_members = ', '.join(in_lab)
+    else:
+        present_members = 'Nobody'
+    if out_lab:
+        home_members = ', '.join(out_lab)
+    else:
+        home_members = 'Nobody'
+
+    return f'Here\'s who\'s in lab: {present_members}\nHere\'s who\'s home: {home_members}'
+
 def _event_handler(event_type, slack_event):
     print(slack_event)
     team_id = slack_event['team_id']
 
     if event_type == 'app_mention':
         ts = slack_event['event']['ts']
+        message_tail = slack_event['event']['text'][:-10]
 
         response = slack_web_client.chat_postMessage(
             channel = 'C013ZC50SPQ',
-            text = f'You mentioned me directly at timestamp {ts}'
+            text = f'I\'m responding to the message that ended "{message_tail}"'
         )
         return make_response('Got the mention', 200,)
+
+    elif event_type == 'message':
+        if slack_event['event']['user'] != 'U014JNM89RP':
+            message_text = slack_event['event']['text'].lower()
+            user_id = slack_event['event']['user']
+            if 'who' in message_text:
+                response = slack_web_client.chat_postMessage(
+                    channel = 'C013ZC50SPQ',
+                    text = who_in_lab()
+                )
+            else:
+                payload = {'token': os.environ['SLACK_BOT_TOKEN'], 'user': user_id}
+                r = requests.get('https://slack.com/api/users.info', params = payload)
+
+                user_name = r['user']['name']
+                in_lab.append(user_name)
+                print(in_lab)
+
+                response = slack_web_client.chat_postMessage(
+                    channel = 'C013ZC50SPQ',
+                    text = f'Got it, checking you in {user_name}'
+                )
+
+        return make_response('Read a message', 200, )
 
 @app.route('/', methods = ['GET', 'POST'])
 def process_request():
