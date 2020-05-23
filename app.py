d9e419f5cbe85e6ef7759bcdc8fc978f7e5acc6c
Rich Posert <rich@posert.com>
2020-05-23 16:31:15 -0700

    Actually revert to in_lab

--- app.py
+++ app.py
@@ -11,38 +11,35 @@ SocketIO(app, cors_allowed_origins='*')
 slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
 
-def who_in_lab(workspace):
-    if not os.path.exists(workspace):
-        os.makedirs(workspace)
+if not os.path.exists('in_lab'):
+    os.makedirs('in_lab')
 
-    present_members = ', '.join([f'<@{x}>' for x in os.listdir(workspace)])
+def who_in_lab():
+    present_members = ', '.join([f'<@{x}>' for x in os.listdir('in_lab')])
     if present_members == '':
         present_members = 'nobody'
 
     return f'Here\'s who\'s in lab: {present_members}'
 
-def check_in(direction, user_name, user_id, workspace):
-    if not os.path.exists(workspace):
-        os.makedirs(workspace)
-
+def check_in(direction, user_name, user_id):
     if direction == 'in':
-        if os.path.exists(os.path.join(workspace, user_id)):
+        if os.path.exists(os.path.join('in_lab', user_id)):
             return f'I already had you present, {user_name}'
         else:
-            open(os.path.join(workspace, user_id), 'a').close()
+            open(os.path.join('in_lab', user_id), 'a').close()
             return f'Got it, checking you in.\n{who_in_lab()}'
     elif direction == 'out':
-        if os.path.exists(os.path.join(workspace, user_id)):
-            os.remove(os.path.join(workspace, user_id))
+        if os.path.exists(os.path.join('in_lab', user_id)):
+            os.remove(os.path.join('in_lab', user_id))
             return f'Hope you got some good work done\n{who_in_lab()}'
         else:
             return f'Can\'t check out if you never came in, {user_name}'
 
 def _event_handler(event_type, slack_event):
+    team_id = slack_event['team_id']
 
     if event_type == 'message':
         if 'bot_id' not in slack_event['event']:
-            team_id = slack_event['team_id']
             message_text = slack_event['event']['text'].lower()
             user_id = slack_event['event']['user']
             user_name = f'<@{user_id}>'
@@ -50,17 +47,17 @@ def _event_handler(event_type, slack_event):
             if 'who' in message_text:
                 response = slack_web_client.chat_postMessage(
                     channel = channel,
-                    text = who_in_lab(team_id)
+                    text = who_in_lab()
                 )
             elif ' in' in message_text:
                 response = slack_web_client.chat_postMessage(
                     channel = channel,
-                    text = check_in('in', user_name, user_id, team_id)
+                    text = check_in('in', user_name, user_id)
                 )
             elif ' out' in message_text:
                 response = slack_web_client.chat_postMessage(
                     channel = channel,
-                    text = check_in('out', user_name, user_id, team_id)
+                    text = check_in('out', user_name, user_id)
                 )
 
         return make_response('Read a message', 200, )
